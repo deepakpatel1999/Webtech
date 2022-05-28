158d6ab2ac9fdbdee6f75f7a06c3c7990685bfe2
deepakpatel1999 <deepak.techwob@gmail.com>
2022-05-29 00:48:48 +0530

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,61 @@
+<?php
+   
+namespace App\Http\Controllers;
+   
+use Illuminate\Http\Request;
+use App\Models\Post;
+   
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+    
+        return view('post.index', compact('posts'));
+    }
+   
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('post.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    	$request->validate([
+            'title'=>'required',
+            'body'=>'required',
+        ]);
+    
+        Post::create($request->all());
+    
+        return redirect()->route('home');
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    	$post = Post::find($id);
+    	
+        return view('post.show', compact('post'));
+    }
+}
